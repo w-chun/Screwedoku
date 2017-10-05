ed06e15b40167cdb34bf69cdb8c3c56d741150d8
App Academy Student <student@appacademy.io>
2017-10-05 16:16:50 -0700

    fixed all bugs

--- sudoku.rb
+++ sudoku.rb
@@ -1,16 +1,16 @@
 require_relative "board"
 require 'colorize'
 
-puts "Only contractors write code this bad.".yellow
+# puts "Only contractors write code this bad."
 
 class SudokuGame
   def self.from_file(filename)
-    board = self.from_file(filename)
+    board = Board.from_file(filename)
     self.new(board)
   end
 
   def initialize(board)
-    @board = [[]]
+    @board = board
   end
 
   def method_missing(method_name, *args)
@@ -29,9 +29,10 @@ class SudokuGame
       print "> "
 
       begin
-        pos = parse_pos(gets)
+        pos = parse_pos(gets.chomp)
+
       rescue
-        TODO: Google how to print the error that happened inside of a rescue statement.
+        #TODO: Google how to print the error that happened inside of a rescue statement.
         puts "Invalid position entered (did you use a comma?)"
         puts ""
 
@@ -41,45 +42,53 @@ class SudokuGame
     pos
   end
 
+
   def get_val
     val = nil
     until val && valid_val?(val)
       puts "Please enter a value between 1 and 9 (0 to clear the tile)"
       print "> "
-      val = parse_val(gets)
+      val = parse_val(gets.chomp)
     end
     val
+  end
+
+  def parse_pos(string)
+    string.split(",").map(&:to_i)
+  end
+
+  def parse_val(string)
+    Integer(string)
   end
 
   def play_turn
     board.render
     pos = get_pos
     val = get_val
-    board[*pos] = val
+    board[pos] = val
   end
 
   def run
     play_turn until solved?
-    board.render
     puts "Congratulations, you win!"
   end
 
   def solved?
-    self.solved?
+    board.grid.flatten.include?(0)
   end
 
   def valid_pos?(pos)
-    if pos.is_a?(:Array) &&
-      pos.length = 2 &&
-      pos.all? { |x| x.in?(0, board.size - 1) }
+    if pos.is_a?(Array) &&
+      pos.length == 2 &&
+      pos.all? { |x| (0..board.size - 1).include?(x) }
       return true
     else
       get_pos
+    end
   end
 
   def valid_val?(val)
-    val.is_a?(Integer) ||
-      val.between?(0, 9)
+    val.is_a?(Integer) || val.between?(0, 9)
   end
 
   private
@@ -88,3 +97,4 @@ end
 
 
 game = SudokuGame.from_file("puzzles/sudoku1.txt")
+game.run
